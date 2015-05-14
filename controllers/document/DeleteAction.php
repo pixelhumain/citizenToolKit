05abc4fd821beffd3838f443b6391cc2c2a681f6
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-05-14 14:12:04 +0400

    Move document to CTK

---
+++ controllers/document/DeleteAction.php
@@ -0,0 +1,32 @@
+<?php
+class DeleteAction extends CAction {
+	
+
+	public function run($dir,$type) {
+		$filepath = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."upload".DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$type.DIRECTORY_SEPARATOR.$_POST['parentId'].DIRECTORY_SEPARATOR.$_POST['name'];
+        if(isset(Yii::app()->session["userId"]) && file_exists ( $filepath ))
+        {
+            if(unlink($filepath))
+            {
+                Document::removeDocumentById($_POST['docId']);
+                echo json_encode(array('result'=>true));
+            }
+            else
+                echo json_encode(array('result'=>false,'error'=>'Something went wrong!'));
+
+            if(isset($_POST['parentId']) && isset($_POST['parentType']) && isset($_POST['pictureKey']) && isset($_POST['path'])){
+            	Document::setImagePath($_POST['parentId'], $_POST['parentType'], "", $_POST['pictureKey']);
+            }
+        } 
+        else 
+        {
+            $doc = Document::getById( $_POST['docId'] );
+            if( $doc )
+                Document::removeDocumentById($_POST['docId']);
+            echo json_encode(array('result'=>false,'error'=>'Something went wrong!',"filepath"=>$filepath));
+        }
+        
+		return Rest::json( Document::save($_POST));
+	}
+
+}
--- models/Document.php
+++ models/Document.php
@@ -182,5 +182,11 @@ class Document {
 
 	}
 
+	public static function getHumanFileSize($bytes, $decimals = 2) {
+      $sz = 'BKMGTP';
+      $factor = floor((strlen($bytes) - 1) / 3);
+      return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+    }
+
 }
 ?>
