34a9826063c246cc63234c93441c3cfc3e95732a
Robert Johnson <zeriam974@gmail.com>
2015-05-15 10:05:28 +0400

    DeleteAction

--- controllers/document/DeleteAction.php
+++ controllers/document/DeleteAction.php
@@ -3,16 +3,16 @@ class DeleteAction extends CAction {
 	
 
 	public function run($dir,$type) {
-		$filepath = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."upload".DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$type.DIRECTORY_SEPARATOR.$_POST['parentId'].DIRECTORY_SEPARATOR.$_POST['name'];
+		$filepath = Yii::app()->params['uploadDir'].$dir.DIRECTORY_SEPARATOR.$type.DIRECTORY_SEPARATOR.$_POST['parentId'].DIRECTORY_SEPARATOR.$_POST['name'];
         if(isset(Yii::app()->session["userId"]) && file_exists ( $filepath ))
         {
             if(unlink($filepath))
             {
                 Document::removeDocumentById($_POST['docId']);
-                echo json_encode(array('result'=>true));
+                echo json_encode(array('result'=>true, "msg" => "Document bien supprimé"));
             }
             else
-                echo json_encode(array('result'=>false,'error'=>'Something went wrong!'));
+                echo json_encode(array('result'=>false,'error'=>'Something went wrong!', "filepath" => $filepath));
 
             /*if(isset($_POST['parentId']) && isset($_POST['parentType']) && isset($_POST['pictureKey']) && isset($_POST['path'])){
             	Document::setImagePath($_POST['parentId'], $_POST['parentType'], "", $_POST['pictureKey']);
@@ -26,7 +26,6 @@ class DeleteAction extends CAction {
             echo json_encode(array('result'=>false,'error'=>'Something went wrong!',"filepath"=>$filepath));
         }
         
-		return Rest::json( Document::save($_POST));
 	}
 
 }
