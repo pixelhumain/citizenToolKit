3d4251755dfccc6c6193269cda5afc8c9698e3ef
Robert Johnson <zeriam974@gmail.com>
2015-05-20 15:06:37 +0400

    CalendarAction

---
+++ controllers/event/CalendarViewAction.php
@@ -0,0 +1,12 @@
+<?php
+	class CalendarViewAction extends CAction{
+
+		public function run(){
+		  	$controller=$this->getController();
+		  	$params = array();
+		  	$events = Event::getWhere($params);
+
+		  	$controller->render("calendarView", array("events" => $events));
+		}
+	}
+?>
