a199354aff00fafafedfb205a487317bb875448c
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-05-27 12:38:27 +0400

    add events for members

--- controllers/pod/SliderAgendaAction.php
+++ controllers/pod/SliderAgendaAction.php
@@ -1,14 +1,33 @@
 <?php
 class SliderAgendaAction extends CAction
 {
+    //Get the three last events that are not passed on time
     public function run($id, $type)
     {
         $controller=$this->getController();
         $params= array();
-        if($type == Organization::COLLECTION){
-			$events = Organization::listEventsPublicAgenda($id);
+        $events=array();
+
+        if($type == Organization::COLLECTION) {
+
+			$listEvent = Event::getListCurrentEventsByOrganizationId($id, 3);
+			
+			//Add information for events
+        	foreach ($listEvent as $key => $value) {
+	        	$profil = Document::getLastImageByKey($key, Event::COLLECTION, Document::IMG_PROFIL);
+	        	if($profil!="")
+	        		$value['imageUrl']=$profil;
+	        	
+	        	//TODO SBAR : add localization display ?
+	        	//@See http://php.net/manual/en/function.strftime.php
+	        	$value["startDate"] = date('Y-m-d h:i:s', $value["startDate"]->sec);
+				$value["endDate"] = date('Y-m-d h:i:s', $value["endDate"]->sec);
+				$events[$key] = $value;
+        	}
+
 			$params["organizationId"] = $id;
 		}
+
 		else if($type == Person::COLLECTION){
 			$events = Authorisation::listEventsIamAdminOf($id);
 		  	$eventsAttending = Event::listEventAttending($id);
@@ -21,20 +40,10 @@ class SliderAgendaAction extends CAction
 		  	$params["itemId"] = $id;
 		}
 
-
-		foreach ($events as $key => $value) {
-			$limit = array(Document::IMG_PROFIL => 1, Document::IMG_MEDIA => 5);
-			$imageUrlTab = Document::getListDocumentsURLByContentKey($key, "event", Document::DOC_TYPE_IMAGE, $limit);
-			if(isset($imageUrlTab[Document::IMG_PROFIL])){
-				$imageUrl = $imageUrlTab[Document::IMG_PROFIL][0];
-				$events[$key]["imageUrl"] = $imageUrl;
-			}
-		}
-
-		
 		$params["eventsAgenda"] = $events;
 		if(isset(Yii::app()->session["userId"]))
 			$params["canEdit"] = Authorisation::canEditItem(Yii::app()->session["userId"], $type, $id);
+		
 		if(Yii::app()->request->isAjaxRequest)
 	        echo $controller->renderPartial("sliderAgenda", $params,true);
 	    else
