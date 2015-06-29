a280fc9aa8979b0bce7b98fd6335bdbdbe39f79d
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-06-29 15:56:23 +0400

    Merge problem

--- controllers/project/SaveAction.php
+++ controllers/project/SaveAction.php
@@ -1,43 +1,18 @@
 <?php
 class SaveAction extends CAction
 {
-    public function run() {
+	public function run() {
 		$controller=$this->getController();
-<<<<<<< HEAD
-		if( isset($_POST['title']) && !empty($_POST['title']))
-		{
-        //TODO check by key
-            $project = PHDB::findOne(PHType::TYPE_PROJECTS ,array( "name" => $_POST['title']));
-            if(!$project)
-            { 
-               //validate isEmail
-               $res = Project::saveProject($_POST);
-               echo json_encode($res);
-            } else
-                   echo json_encode(array("result"=>false, "msg"=>"Ce projet existe déjà."));
-<<<<<<< HEAD
-    	} else
-        	echo json_encode(array("result"=>false, "msg"=>"Ce projet doit avoir un nom."));
-=======
-    } else
-        echo json_encode(array("result"=>false, "msg"=>Yii::t("common", "Uncorrect request")));
->>>>>>> 3e3619a3243147747239972d8930bf2c7455b784
-    exit;
 
-	}
-=======
-		
 		if (isset(Yii::app()->session["userId"])) {
-		    try {
+			try {
 				$res = Project::insert($_POST, Yii::app()->session["userId"]);
-		    } catch (CTKException $e) {
+			} catch (CTKException $e) {
 				$res = array("result"=>false, "msg"=>$e->getMessage());
 			}
 			Rest::json($res);
 		} else {
 			$res = array("result"=>false, "msg"=>"You must be connected to create a project");
 		}
-    
-   	}
->>>>>>> 527b60aa6ce242acfa48bcdaabffabb0d27f4e41
+	}
 }
