c48f3bf9387915a6bdfbb3b7e538acbe1ff3c893
Robert Johnson <zeriam974@gmail.com>
2015-06-30 09:50:46 +0400

    get cityId in pod photoVideo

--- controllers/pod/PhotoVideoAction.php
+++ controllers/pod/PhotoVideoAction.php
@@ -1,14 +1,26 @@
 <?php
 class PhotoVideoAction extends CAction
 {
-    public function run($id, $type)
+    public function run($id=null, $type, $insee=null)
     {
         $controller=$this->getController();
         $params = array();
 		$params["type"] = $type;
-		$params["itemId"] = $id;
-		if(isset(Yii::app()->session["userId"]))
+		if(isset($id)){
+			$params["itemId"] = $id;
+			if(isset(Yii::app()->session["userId"]))
 			$params["canEdit"] = Authorisation::canEditItem(Yii::app()->session["userId"], $type, $id);
+		}else if (isset($insee)){
+			$params["insee"] = $insee;
+			$where = array("insee" => $insee);
+			$city = City::getWhere($where);
+			foreach ($city as $key => $value) {
+				$id = $value["_id"];
+			}
+			$params["itemId"] = $id;
+		}
+
+		
 		if(Yii::app()->request->isAjaxRequest)
 	        echo $controller->renderPartial("photoVideo", $params,true);
 	    else
