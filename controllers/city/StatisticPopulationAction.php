759411e9a825e3d0a1208c94881b76fbcc5c7d91
Raphael RIVIERE <raphael_riviere@yahoo.fr>
2015-07-13 15:35:01 +0400

    mise a jour de la visualisation des données

--- controllers/city/StatisticPopulationAction.php
+++ controllers/city/StatisticPopulationAction.php
@@ -8,24 +8,21 @@ class StatisticPopulationAction extends CAction
     	$fields = array("codeInsee.".$insee);*/
        	$where = array("insee"=>$insee, $typeData => array( '$exists' => 1 ));
     	$fields = array();
+
     	
     	$cityData = City::getWhereData($where, $fields);
 		$where = array("insee" => $insee);
 		$fields = array("name");
 
-		$params["cityData2"] = $cityData;
-
 		$city = City::getWhere($where, $fields);
 		foreach ($city as $key => $value) {
 			$name = $value["name"];
 		}
-
 		/*foreach ($cityData as $key => $value) {
 			foreach ($value as $k => $v) {
 				$cityData = array($name => $v);
 			}
 		}*/
-
 		foreach ($cityData as $key => $value) {
 			foreach ($value as $k => $v) {
 				if($k == "population")
@@ -33,9 +30,13 @@ class StatisticPopulationAction extends CAction
 			}
 		}
 
+		$params["nbCitiesDepartement"] = count(City::getDepartementCitiesByInsee($insee));
+		$params["nbCitiesRegion"] = count(City::getRegionCitiesByInsee($insee));
     	$params["cityData"] = $cityData;
 
         $params["title"] = "Population/An";
+
+
         if(Yii::app()->request->isAjaxRequest)
 	        echo $controller->renderPartial("statistiquePop", $params,true);
 	    else
