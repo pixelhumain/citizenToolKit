7d2ebcbe254688e43fecfb6ac7dfe569afe50fe4
Clément Damiens <clement.damiens@gmail.com>
2015-09-09 17:30:58 +0200

    Add event to the timeline

--- controllers/gantt/IndexAction.php
+++ controllers/gantt/IndexAction.php
@@ -1,7 +1,7 @@
 <?php
 class IndexAction extends CAction
 {
-    public function run( $type=null, $id= null, $year= null )
+    public function run( $type=null, $id= null, $year= null, $isAdmin=null )
     {
 	    function array_msort($array, $cols)
 		{
@@ -39,8 +39,45 @@ class IndexAction extends CAction
             $controller->title = $project["name"]."'s Timeline";
             $controller->subTitle = "Every Project has steps to get over.";
             $controller->pageTitle = "Communecter - ".$controller->title;
+            // If event, we add it to the timeline
+			if (isset($project["links"]["events"])){
+				$taskEvent=[];
+				$newArrayEvent=[];
+				foreach ($project["links"]["events"] as $idEvent => $e) {
+					$eventsProject=Event::getById($idEvent);
+					array_push($taskEvent,$eventsProject);	
+				}
+				if (isset($year)){
+			        foreach ($taskEvent as $key => $val){
+				        $startDate=date("Y",strtotime($val["startDate"]));
+				        $endDate=date("Y",strtotime($val["endDate"]));
+				        if ($startDate==$year || (($startDate<$year) && ($endDate>=$year))){	
+					        if ($startDate<$year)
+								$startDate=$year.'-01-01';
+					        else
+					        	$startDate=date("Y-m-d",strtotime($val["startDate"]));
+					        if ($endDate > $year)
+					        	$endDate=$year.'-12-31';
+					        else
+					        	$endDate=date("Y-m-d",strtotime($val["endDate"]));
+					        $valEv=array("color"=>"lorem","name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=>"event");
+							$newArrayEvent[]=$valEv;
+						}
+			        }
+			        $taskEvent=$newArrayEvent;
+				}
+				else{
+					foreach ($taskEvent as $key => $val){
+						$keyEvent=(string)$val["_id"];
+						$startDate=date("Y-m-d",strtotime($val["startDate"]));
+				        $endDate=date("Y-m-d",strtotime($val["endDate"]));
+						$valEv=array("color"=>"lorem","name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=>"event");
+						 $newArrayEvent[]=$valEv;
+					}
+					$taskEvent=$newArrayEvent;
+				}
+			}
         } 
-         
         else if( $type == Event::COLLECTION ) {
             $controller->toolbarMBZ = array("<a href='".Yii::app()->createUrl("/".$controller->module->id."/event/dashboard/id/".$id)."'><i class='fa fa-lightbulb-o'></i>Event</a>");
             $event = Event::getById($id);
@@ -49,17 +86,18 @@ class IndexAction extends CAction
             $controller->pageTitle = "Communecter - ".$controller->title;
         } 
         /*  CAN EDIT OR NOT   */
-		$edit = Authorisation :: canEditItem(Yii::app()->session["userId"], $type, $id);
+		//$edit = Authorisation :: canEditItem(Yii::app()->session["userId"], $type, $id);
 		///////////////////////
         array_push( $controller->toolbarMBZ, '<a href="#" class="newRoom" title="proposer une " ><i class="fa fa-plus"></i> Room </a>');
-		
+        
         $where = array(
                 "_id"=>new MongoId($id),
                 "tasks" =>  array('$exists' => 1));
   		$tasks = Gantt::getTasks($where,$type);
+  	
+  		$newArray=[];
         if (isset($year)){
 	        $period=$year;
-	        $newArray=[];
 	        foreach ($tasks as $key => $val){
 		        $startDate=date("Y",strtotime($val["startDate"]));
 		        $endDate=date("Y",strtotime($val["endDate"]));
@@ -72,22 +110,31 @@ class IndexAction extends CAction
 			        	$endDate=$year.'-12-31';
 			        else
 			        	$endDate=$val["endDate"];
-			        $val=array("color"=>$val["color"],"name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate);
-					$newArray[$key]=$val;
+			        $val=array("color"=>$val["color"],"name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=>$key);
+					$newArray[]=$val;
 				}
 	        }
-	        $tasks=$newArray;
 		}
 		else{
 			$period="yearly";
+			foreach ($tasks as $key => $val){
+				$startDate=date("Y-m-d",strtotime($val["startDate"]));
+		        $endDate=date("Y-m-d",strtotime($val["endDate"]));
+				$val=array("color"=>$val["color"],"name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=>$key);
+				$newArray[]=$val;
+			}
 		}
-		
+		//Mergin of event line and task
+		if (isset($taskEvent))
+			$tasksFinal=array_merge_recursive($newArray, $taskEvent);
+		else 
+			$tasksFinal=$newArray;
 		/* Tri des taches par ordre de date croissante*/
-		$tasks2 = array_msort($tasks, array('startDate'=>SORT_ASC, 'endDate'=>SORT_ASC));
+		$tasks2 = array_msort($tasksFinal, array('startDate'=>SORT_ASC, 'endDate'=>SORT_ASC));
 
 // Trie les données par volume décroissant, edition croissant
 // Ajoute $data en tant que dernier paramètre, pour trier par la clé commune
-		$params = array("tasks" => $tasks2,  "period" => $period, "edit" => $edit);
+		$params = array("tasks" => $tasks2,  "period" => $period, "edit" => $isAdmin);
 		if(Yii::app()->request->isAjaxRequest)
 	        echo $controller->renderPartial("index", $params,true);
 	    else
