8865d2585a2aa24c2a19e0b0b05a741a4502bb55
Kgneo <tristan.goguet@gmail.com>
2015-09-25 17:30:39 +1100

    ajout du type dans la data retournée

--- controllers/sig/GetMyPositionAction.php
+++ controllers/sig/GetMyPositionAction.php
@@ -6,16 +6,23 @@ class GetMyPositionAction extends CAction
         $where = array(	'_id'  => new MongoId(Yii::app()->session["userId"]) );
 	 	$user = PHDB::find(Person::COLLECTION, $where);
     	 
-    	 //si l'utilisateur connecté n'a pas enregistré sa position geo
-    	 //on prend la position de son CP
-    	 foreach($user as $me)
-    	 if(!isset($me["geo"]) && isset($me["cp"])) {
-    		$res = array(Yii::app()->session["userId"] => 
-						SIG::getGeoPositionByCp($me["cp"]));
-			Rest::json( $res );
-			Yii::app()->end();
+    	//si l'utilisateur connecté n'a pas enregistré sa position geo
+    	//on prend la position de son CP
+    	foreach($user as $me){
+
+            $type = isset($me["type"]) ? $me["type"] : "citoyen";
+                
+        	if(!isset($me["geo"]) && isset($me["cp"])) {
+                $res = array("position" => SIG::getPositionByCp($me["cp"]), "type" => $type);
+    			Rest::json( $res );
+    			Yii::app()->end();
+            }
+            else{
+                $res = array("position" => $me["geo"], "type" => $type);
+                Rest::json( $res );
+                Yii::app()->end();
+            }
         }
-		Rest::json( $user );
-		Yii::app()->end();
+		
     }
 }
