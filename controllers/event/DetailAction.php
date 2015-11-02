40bb25d112db7783fbcaf9d6dee2f776de398b45
Kgneo <tristan.goguet@gmail.com>
2015-11-03 00:52:51 +1100

    up toolbar position & labels & tooltips

--- controllers/event/DetailAction.php
+++ controllers/event/DetailAction.php
@@ -34,7 +34,7 @@ class DetailAction extends CAction
                 array_push($attending, $citoyen);
 
                 if( $uid == Yii::app()->session['userId'] )
-                    array_push($controller->toolbarMBZ, array('tooltip' => "Send a message to this Event","iconClass"=>"fa fa-envelope-o","href"=>"<a href='#' class='new-news' data-id='".$id."' data-type='".Event::COLLECTION."' data-name='".$event['name']."'") );
+                    array_push($controller->toolbarMBZ, array('position'=>'left', 'label'=>'Contact', 'tooltip' => "Send a message to this Event","iconClass"=>"fa fa-envelope-o","href"=>"<a href='#' class='new-news tooltips btn btn-default' data-id='".$id."' data-type='".Event::COLLECTION."' data-name='".$event['name']."'") );
               }
 
               /*if(isset($e["isAdmin"]) && $e["isAdmin"]==true){
@@ -77,9 +77,9 @@ class DetailAction extends CAction
         }
 
         if(isset($event["_id"]) && isset(Yii::app()->session["userId"]) && Link::isLinked($event["_id"] , Event::COLLECTION , Yii::app()->session['userId']))
-            array_push($controller->toolbarMBZ, array('tooltip' => "leave this Event", "parent"=>"span","parentId"=>"linkBtns","iconClass"=>"disconnectBtnIcon fa fa-unlink","href"=>"<a href='javascript:;' class='disconnectBtn text-red tooltips btn btn-default'  data-name='".$event["name"]."' data-id='".$event["_id"]."' data-type='".Event::COLLECTION."' data-member-id='".Yii::app()->session["userId"]."' data-ownerlink='".Link::person2events."' data-targetlink='".Link::event2person."'") );
+            array_push($controller->toolbarMBZ, array('position'=>'right', 'label'=>'Leave', 'tooltip' => "leave this Event", "parent"=>"span","parentId"=>"linkBtns","iconClass"=>"disconnectBtnIcon fa fa-unlink","href"=>"<a href='javascript:;' class='disconnectBtn text-red tooltips btn btn-default'  data-name='".$event["name"]."' data-id='".$event["_id"]."' data-type='".Event::COLLECTION."' data-member-id='".Yii::app()->session["userId"]."' data-ownerlink='".Link::person2events."' data-targetlink='".Link::event2person."'") );
 		else
-			array_push($controller->toolbarMBZ, array('tooltip' => "join this Event", "parent"=>"span","parentId"=>"linkBtns","iconClass"=>"connectBtnIcon fa fa-unlink","href"=>"<a href='javascript:;' class='connectBtn tooltips ' id='addKnowsRelation' data-placement='top' data-ownerlink='".Link::person2events."' data-targetlink='".Link::event2person."' ") );
+			array_push($controller->toolbarMBZ, array('position'=>'right', 'label'=>'Join','tooltip' => "join this Event", "parent"=>"span","parentId"=>"linkBtns","iconClass"=>"connectBtnIcon fa fa-unlink","href"=>"<a href='javascript:;' class='connectBtn tooltips ' id='addKnowsRelation' data-placement='top' data-ownerlink='".Link::person2events."' data-targetlink='".Link::event2person."' ") );
 
         $params["images"] = $images;
         $params["contentKeyBase"] = $contentKeyBase;
