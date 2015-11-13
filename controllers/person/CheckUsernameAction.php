22eb2053a4c54ca6f79c9e3ee304c5d1f36b7f99
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-11-13 10:54:02 +0400

    add username for a person

---
+++ controllers/person/CheckUsernameAction.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Check if the username is unique in the db.
+*/
+class CheckUsernameAction extends CAction {
+    
+    public function run() {
+    	$username = @$_POST["username"];
+        assert('!empty($username); //The username is mandatory');
+    	
+        //TODO SBAR - return a list of username available
+        Rest::json(Person::checkUniqueUsername($username));
+    }
+}
