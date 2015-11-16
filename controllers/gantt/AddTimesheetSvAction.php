d0a119c25e8993ce90c008e3fbeef2d26e9c8aef
Clément Damiens <clement.damiens@gmail.com>
2015-11-16 15:42:25 +0100

    add parent in addTimesshet for menu

--- controllers/gantt/AddTimesheetSvAction.php
+++ controllers/gantt/AddTimesheetSvAction.php
@@ -1,7 +1,7 @@
 <?php
 class AddTimesheetSvAction extends CAction
 {
-    public function run(){
+    public function run($id = null, $type = null){
 
     	$controller=$this->getController();
     	//$params = array();
@@ -11,6 +11,8 @@ class AddTimesheetSvAction extends CAction
             $tasks = str_replace('\"','"',$_GET["tasks"]);
             //echo $tasks;
 			$params["tasks"] = unserialize(base64_decode($tasks));
+            $params["project"]  = Project::getPublicData($id);
+
 			//print_r($_GET["tasks"]);
         }
         if(Yii::app()->request->isAjaxRequest){
