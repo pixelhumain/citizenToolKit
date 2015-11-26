4c9a68a98a0e285ab4d83738a62a9e84509f6514
Tibor Katelbach <oceatoon@gmail.com>
2015-11-26 18:32:15 +0400

    removing project userId test

--- controllers/project/SaveAction.php
+++ controllers/project/SaveAction.php
@@ -4,15 +4,15 @@ class SaveAction extends CAction
 	public function run($id,$type) {
 		$controller=$this->getController();
 
-		if (isset(Yii::app()->session["userId"])) {
+		//if (isset(Yii::app()->session["userId"])) {
 			try {
 				$res = Project::insert($_POST, $id,$type);
 			} catch (CTKException $e) {
 				$res = array("result"=>false, "msg"=>$e->getMessage());
 			}
 			Rest::json($res);
-		} else {
+		/*} else {
 			$res = array("result"=>false, "msg"=>"You must be connected to create a project");
-		}
+		}*/
 	}
 }
