ab6ca62f82e0fd2dd0f63d7188a1fe279cffe635
Kgneo <tristan.goguet@gmail.com>
2016-01-26 21:00:21 +1100

    maj global search page

--- controllers/search/GlobalAutoCompleteAction.php
+++ controllers/search/GlobalAutoCompleteAction.php
@@ -8,13 +8,11 @@ class GlobalAutoCompleteAction extends CAction
         
         /***********************************  DEFINE GLOBAL QUERY   *****************************************/
         $query = array( "name" => new MongoRegex("/".$search."/i"));
-  		//error_log("start search width locality : " . $locality);
-        
+  		
 
         /***********************************  TAGS   *****************************************/
         if(strpos($search, "#") === 0){
         	$search = substr($search, 1, strlen($search));
-        	//error_log($search);
         	$query = array( "tags" => array('$in' => array(new MongoRegex("/".$search."/i")))) ; //new MongoRegex("/".$search."/i") )));
   		}
 
@@ -22,7 +20,6 @@ class GlobalAutoCompleteAction extends CAction
   		/***********************************  DEFINE LOCALITY QUERY   *****************************************/
         if($locality != null && $locality != ""){
 
-        	//error_log("search use locality : " . $locality);
         	$type = $this->getTypeOfLocalisation($locality);
         	$queryLocality = array();
         	
@@ -30,132 +27,115 @@ class GlobalAutoCompleteAction extends CAction
         		$queryLocality = array("address.addressLocality" => new MongoRegex("/".$locality."/i"));
         	}
         	if($type == "CODE_POSTAL_INSEE") {
-        		// $queryLocality = array('$or' => 
-        		// 			array("address.postalCode" => $locality, 
-        		// 				  "address.codeInsee" => $locality) );
         		$queryLocality = array("address.postalCode" => $locality );
         	}
         	if($type == "DEPARTEMENT") {
-        		
-        		/*$zero = strlen($locality) > 2 ? "00" : "000";
-        		$localityMax = intval($locality) + 1;
-        		$locality = $locality . $zero;
-        		$localityMax = $localityMax . $zero;
-				*/
         		$queryLocality = array("address.postalCode" 
 						=> new MongoRegex("/".$locality."/i"));
         	}
         	$query = array('$and' => array($query, $queryLocality) );
 	    }
 	  	
-	  	if($search != null && $search != ""){
-		  	
+  	
 	        $res = array();
 
 
-	        /***********************************  PERSONS   *****************************************/
-	        if(strcmp($filter, Person::COLLECTION) != 0){
+        /***********************************  PERSONS   *****************************************/
+        if(strcmp($filter, Person::COLLECTION) != 0){
 
-	        	$allCitoyen = PHDB::find ( Person::COLLECTION , $query, array("name", "address", "shortDescription", "description"));
+        	$allCitoyen = PHDB::find ( Person::COLLECTION , $query, array("name", "address", "shortDescription", "description"));
 
-		  		foreach ($allCitoyen as $key => $value) {
-		  			$person = Person::getSimpleUserById($key);
-		  			$person["type"] = "citoyen";
-					$allCitoyen[$key] = $person;
-		  		}
+	  		foreach ($allCitoyen as $key => $value) {
+	  			$person = Person::getSimpleUserById($key);
+	  			$person["type"] = "citoyen";
+				$allCitoyen[$key] = $person;
+	  		}
 
-		  		$res["citoyen"] = $allCitoyen;
+	  		$res["citoyen"] = $allCitoyen;
 
-		  	}
+	  	}
 
-		  	/***********************************  ORGANISATIONS   *****************************************/
-	        if(strcmp($filter, Organization::COLLECTION) != 0){
+	  	/***********************************  ORGANISATIONS   *****************************************/
+        if(strcmp($filter, Organization::COLLECTION) != 0){
 
-		  		$allOrganizations = PHDB::find ( Organization::COLLECTION ,$query ,array("name", "address", "shortDescription", "description"));
-		  		foreach ($allOrganizations as $key => $value) {
-		  			$orga = Organization::getSimpleOrganizationById($key);
-					$orga["type"] = "organization";
-					$allOrganizations[$key] = $orga;
-		  		}
+	  		$allOrganizations = PHDB::find ( Organization::COLLECTION ,$query ,array("name", "address", "shortDescription", "description"));
+	  		foreach ($allOrganizations as $key => $value) {
+	  			$orga = Organization::getSimpleOrganizationById($key);
+				$orga["type"] = "organization";
+				$allOrganizations[$key] = $orga;
+	  		}
 
-		  		$res["organization"] = $allOrganizations;
-		  	}
+	  		$res["organization"] = $allOrganizations;
+	  	}
 
-		  	/***********************************  EVENT   *****************************************/
-	        if(strcmp($filter, Event::COLLECTION) != 0){
-		  		$allEvents = PHDB::find(PHType::TYPE_EVENTS, $query, array("name", "address", "shortDescription", "description"));
-		  		foreach ($allEvents as $key => $value) {
-		  			$event = Event::getSimpleEventById($key);
-					$event["type"] = "event";
-					$allEvents[$key] = $event;
-		  		}
-		  		
-		 
-		  		$res["event"] = $allEvents;
-		  	}
+	  	/***********************************  EVENT   *****************************************/
+        if(strcmp($filter, Event::COLLECTION) != 0){
+	  		$allEvents = PHDB::find(PHType::TYPE_EVENTS, $query, array("name", "address", "shortDescription", "description"));
+	  		foreach ($allEvents as $key => $value) {
+	  			$event = Event::getById($key);
+				$event["type"] = "event";
+				$allEvents[$key] = $event;
+	  		}
+	  		
+	 
+	  		$res["event"] = $allEvents;
+	  	}
 
-		  	/***********************************  PROJECTS   *****************************************/
-	        if(strcmp($filter, Project::COLLECTION) != 0){
-		  		$allProject = PHDB::find(Project::COLLECTION, $query, array("name", "address", "shortDescription", "description"));
-		  		foreach ($allProject as $key => $value) {
-		  			$project = Project::getSimpleProjectById($key);
-					$project["type"] = "project";
-					$allProject[$key] = $project;
-		  		}
-		  		$res["project"] = $allProject;
-		  	}
-
-		}
+	  	/***********************************  PROJECTS   *****************************************/
+        if(strcmp($filter, Project::COLLECTION) != 0){
+	  		$allProject = PHDB::find(Project::COLLECTION, $query, array("name", "address", "shortDescription", "description"));
+	  		foreach ($allProject as $key => $value) {
+	  			$project = Project::getById($key);
+				$project["type"] = "project";
+				$allProject[$key] = $project;
+	  		}
+	  		$res["project"] = $allProject;
+	  	}
+	  	
 
 	  	/***********************************  CITIES   *****************************************/
         if(strcmp($filter, City::COLLECTION) != 0){
 	  		$query = array( "name" => new MongoRegex("/".self::wd_remove_accents($search)."/i"));
-	  		//$query = array('$and' => array($query, $queryLocality) );
-
+	  		
 	  		/***********************************  DEFINE LOCALITY QUERY   *****************************************/
 	        if($locality != null && $locality != ""){
 
-	        	//error_log("search use locality for Cities : " . $locality . " - Type : " . $type);
-	        	//$queryLocality = array();
-	        	
 	        	if($type == "NAME"){ 
 	        		$query = array("name" => new MongoRegex("/".self::wd_remove_accents($locality)."/i"));
 	        	}
 	        	if($type == "CODE_POSTAL_INSEE") {
-	        		// $queryLocality = array('$or' => 
-	        		// 			array("address.postalCode" => $locality, 
-	        		// 				  "address.codeInsee" => $locality) );
 	        		$query = array("cp" => $locality );
 	        	}
 	        	if($type == "DEPARTEMENT") {
-	        		
-	        		////error_log($locality . " | localityMax : " . $localityMax);
-	      //   		$query = array("cp" 
-							// => array('$lt' => intval($localityMax),
-							// 		 '$gt' => intval($locality) 
-							// 		 ));
 	        		$query = array("dep" => $locality );
 	        	}
-	        	//$query = array('$and' => array($query, $queryLocality) );
 		    }
 
 	  		$allCities = PHDB::find(City::COLLECTION, $query, array("name", "cp", "insee", "geo"));
+	  		$allCitiesRes = array();
+	  		$nbMaxCities = 20;
+	  		$nbCities = 0;
 	  		foreach ($allCities as $key => $value) {
-	  			$city = City::getSimpleCityById($key);
-	  			$city["type"] = "city";
-				$allCities[$key] = $city;
+	  			if($nbCities < $nbMaxCities){
+		  			$city = City::getSimpleCityById($key);
+		  			$city["type"] = "city";
+					$allCitiesRes[$key] = $city;
+				} $nbCities++;
 	  		}
-	  		$res["cities"] = $allCities;
+	  		$res["cities"] = $allCitiesRes;
 
 	  		if(empty($res["cities"])){
 	  			$query = array( "cp" => $search);
 		  		$allCities = PHDB::find(City::COLLECTION, $query, array("name", "cp", "insee", "geo"));
-		  		foreach ($allCities as $key => $value) {
-		  			$city = City::getSimpleCityById($key);
-					$city["type"] = "city";
-					$allCities[$key] = $city;
+		  		$nbCities = 0;
+	  			foreach ($allCities as $key => $value) {
+		  			if($nbCities < $nbMaxCities){
+			  			$city = City::getSimpleCityById($key);
+						$city["type"] = "city";
+						$allCitiesRes[$key] = $city;
+					} $nbCities++;
 		  		}
-		  		$res["cities"] = $allCities;  		
+		  		$res["cities"] = $allCitiesRes;  		
 	  		}
 	  	}
 
