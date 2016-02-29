6636e898bf3ef97d1292a49752893f95f10c2e25
Clément Damiens <clement.damiens@gmail.com>
2016-02-29 10:09:42 +0100

    add updateConfidentiality

--- models/Preference.php
+++ models/Preference.php
@@ -7,4 +7,32 @@ class Preference {
 		$res = array("result" => true, "msg" => Yii::t("common","Your request is well updated"));
 		return $res;
 	}
+	
+	public static function updateConfidentiality($id, $type, $param){
+		if ($type == Person::COLLECTION)
+			$context = Person::getById($id);
+			
+		$setType = $param["type"]; 
+		$setValue = $param["value"];
+		
+		if($setValue == "hide"){
+			PHDB::update($type, array("_id" => new MongoId($id)), 
+			    array('$unset' => array("preferences.publicFields.".$setType => "","preferences.privateFields.".$setType => "")
+			));
+
+		} else if($setValue == "private"){
+			PHDB::update($type, array("_id" => new MongoId($id)), 
+			     array('$set'=> array("preferences.privateFields.".$setType => $setType), '$unset' => array("preferences.publicFields.".$setType => ""))
+			);
+			
+		} else if($setValue == "public"){
+			PHDB::update($type, array("_id" => new MongoId($id)), 
+			    array('$unset' => array("preferences.privateFields.".$setType => ""),'$set'=> array("preferences.publicFields.".$setType => $setType))
+			);
+		}
+		$res = array("result" => true, "msg" => Yii::t("common","Confidentiality param well updated"));
+		return $res;
+
+		
+	}
 }
