db6fbf94432edb3e5d0c5a24a35efe76a156ced7
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-03-05 00:46:38 +0400

    last update on project to fix date

--- models/Project.php
+++ models/Project.php
@@ -39,23 +39,25 @@ class Project {
 	  	if ($project !=null) {
 		  	if (!empty($project["startDate"]) || !empty($project["endDate"])) {
 		  		if(isset($project["startDate"]) && isset($project["endDate"])) {
+					$now = time();
+					$yesterday = mktime(0, 0, 0, date("m")  , date("d")-1, date("Y"));
+					$yester2day = mktime(0, 0, 0, date("m")  , date("d")-2, date("Y"));
+
 					if (gettype($project["startDate"]) == "object") {
 						//Set TZ to UTC in order to be the same than Mongo
 						date_default_timezone_set('UTC');
 						if (!empty($project["startDate"]))
 							$project["startDate"] = date('Y-m-d H:i:s', $project["startDate"]->sec);
+					} else {
+						$project["startDate"] = date('Y-m-d H:i:s',$yester2day);;
 					}
+					
 					if (gettype($project["endDate"]) == "object") {
 						date_default_timezone_set('UTC');
 						if (!empty($project["endDate"]))
 							$project["endDate"] = date('Y-m-d H:i:s', $project["endDate"]->sec);
 					} else {
-						//Manage old date with string on date project
-						$now = time();
-						$yesterday = mktime(0, 0, 0, date("m")  , date("d")-1, date("Y"));
-						$yester2day = mktime(0, 0, 0, date("m")  , date("d")-2, date("Y"));
 						$project["endDate"] = date('Y-m-d H:i:s', $yesterday);
-						$project["startDate"] = date('Y-m-d H:i:s',$yester2day);;
 					}
 				}
 			}
