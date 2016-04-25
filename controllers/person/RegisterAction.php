012a55c34de3345dd4c09e8007173d460d71b186
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-04-25 17:59:39 +0400

    change message when connecting

--- controllers/person/RegisterAction.php
+++ controllers/person/RegisterAction.php
@@ -75,7 +75,9 @@ class RegisterAction extends CAction
 			//TODO
 			//send communecter overview mail
 			//Mail::communecterOverview($newPerson);
-			$res = array("result"=>true, "msg"=> Yii::t("login","You are now communnected !")."<br>".Yii::t("login","Our developpers are fighting to open soon ! Check your mail that will happen soon !"), "id"=>$pendingUserId); 
+			$res = array("result"=>true, 
+				"msg"=> Yii::t("login","You are now communnected !")."<br>".Yii::t("login","Our developpers are fighting to open soon ! Check your mail that will happen soon !")."<br>".Yii::t("login","If you really want to start testing the platform before, send us an email and we'll consider your demand :)"), 
+				"id"=>$pendingUserId); 
 		} 
 		Rest::json($res);
 		exit;
