72c9abf6f53ae86e4f5e6f05e65b231925879cc5
raphael <raphael_riviere@yahoo.fr>
2016-04-29 10:01:30 +0400

    Correction Bug : Maintenant on ne peut acceder a la page adminpublic si on n'a pas les droits

--- controllers/adminpublic/IndexAction.php
+++ controllers/adminpublic/IndexAction.php
@@ -5,12 +5,20 @@ class IndexAction extends CAction
     public function run($key)
     {
     	$controller=$this->getController();
-        $params["entitiesSourceAdmin"] = Import::getAllEntitiesByKey($key);
-        //$params = array();
+        $page = "../error/error";
+        if(Role::isSourceAdmin(Role::getRolesUserId(Yii::app()->session["userId"]))){
+            if(Person::getSourceAdmin(Yii::app()->session["userId"])){
+                $params["entitiesSourceAdmin"] = Import::getAllEntitiesByKey($key);
+                $page = "index";
+            }
+        }
+
+
         if(Yii::app()->request->isAjaxRequest)
-            echo $controller->renderPartial("index",$params,true);
-        else 
-            $controller->render("index",$params); 
+                echo $controller->renderPartial("index",$params,true);
+            else 
+                $controller->render("index",$params);
+         
         
     }
 }
