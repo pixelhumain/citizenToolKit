87b2470e11cbdfe74ee21987d0ecba611ada3e43
Childee <childericthoreau@gmail.com>
2016-05-19 18:44:56 +0400

    Links graph with listRoomTypes

--- controllers/stat/ChartGlobalAction.php
+++ controllers/stat/ChartGlobalAction.php
@@ -13,7 +13,7 @@ class ChartGlobalAction extends CAction
 		$params = array();
 
 		//We have to send data names to group to the charts
-		$params['groups'] = Lists::get(array('organisationTypes', 'eventTypes', 'actionRoomsTypes'));
+		$params['groups'] = Lists::get(array('organisationTypes', 'eventTypes', 'listRoomTypes'));
 		$page =  "chartGlobal";
 
 		if(Yii::app()->request->isAjaxRequest){
