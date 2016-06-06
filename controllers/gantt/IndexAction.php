5c2ce68a956b7fcce10741fb20a9c88fc51c8368
Tibor Katelbach <oceatoon@gmail.com>
2016-06-06 21:41:46 +0400

    added timeline when subevents

--- controllers/gantt/IndexAction.php
+++ controllers/gantt/IndexAction.php
@@ -29,16 +29,11 @@ class IndexAction extends CAction
 		}
         $controller=$this->getController();
         
-        $controller->title = "Action Tasks";
-        $controller->subTitle = "Define tasks in order to show what's next to the community";
-        $controller->pageTitle = "Communecter - Action Tasks";
         
         if( $type == Project::COLLECTION ) {
             $controller->toolbarMBZ = array("<a href='".Yii::app()->createUrl("/".$controller->module->id."/project/dashboard/id/".$id)."'><i class='fa fa-lightbulb-o'></i>Project</a>");
             $project = Project::getById($id);
-            $controller->title = $project["name"]."'s Timeline";
-            $controller->subTitle = "Every Project has steps to get over.";
-            $controller->pageTitle = "Communecter - ".$controller->title;
+
             // If event, we add it to the timeline
 			if (isset($project["links"]["events"])){
 				$taskEvent=array();
@@ -81,14 +76,44 @@ class IndexAction extends CAction
         else if( $type == Event::COLLECTION ) {
             $controller->toolbarMBZ = array("<a href='".Yii::app()->createUrl("/".$controller->module->id."/event/dashboard/id/".$id)."'><i class='fa fa-lightbulb-o'></i>Event</a>");
             $event = Event::getById($id);
-            $controller->title = $event["name"]."'s Timeline";
-            $controller->subTitle = "Every event has steps to get over.";
-            $controller->pageTitle = "Communecter - ".$controller->title;
+            $subEvents = PHDB::find(Event::COLLECTION,array("parentId"=>$id));
+
+            $taskEvent=array($event);
+			$newArrayEvent=array();
+			foreach ($subEvents as $idEvent => $e) {
+				$eventsProject=Event::getById($idEvent);
+				array_push($taskEvent,$eventsProject);	
+			}
+			if (isset($year)){
+		        foreach ($taskEvent as $key => $val){
+			        $startDate=date("Y",strtotime($val["startDate"]));
+			        $endDate=date("Y",strtotime($val["endDate"]));
+			        if ($startDate==$year || (($startDate<$year) && ($endDate>=$year))){	
+				        if ($startDate<$year)
+							$startDate=$year.'-01-01';
+				        else
+				        	$startDate=date("Y-m-d",strtotime($val["startDate"]));
+				        if ($endDate > $year)
+				        	$endDate=$year.'-12-31';
+				        else
+				        	$endDate=date("Y-m-d",strtotime($val["endDate"]));
+				        $valEv=array("color"=>"lorem","name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=> array ("address"=> $val["address"]["addressLocality"]));
+						$newArrayEvent[]=$valEv;
+					}
+		        }
+		        $taskEvent=$newArrayEvent;
+			}
+			else{
+				foreach ($taskEvent as $key => $val){
+					$keyEvent=(string)$val["_id"];
+					$startDate=date("Y-m-d",strtotime($val["startDate"]));
+			        $endDate=date("Y-m-d",strtotime($val["endDate"]));
+					$valEv=array("color"=>"lorem","name"=>$val["name"],"startDate"=>$startDate,"endDate"=>$endDate,"key"=> array ("address"=> $val["address"]["addressLocality"]));
+					 $newArrayEvent[]=$valEv;
+				}
+				$taskEvent=$newArrayEvent;
+			}
         } 
-        /*  CAN EDIT OR NOT   */
-		//$edit = Authorisation :: canEditItem(Yii::app()->session["userId"], $type, $id);
-		///////////////////////
-        array_push( $controller->toolbarMBZ, '<a href="#" class="newRoom" title="proposer une " ><i class="fa fa-plus"></i> Room </a>');
         
         $where = array(
                 "_id"=>new MongoId($id),
