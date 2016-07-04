d4706420a0555fa5b0cc9b24b9bb6d9225aa1a38
Tibor Katelbach <oceatoon@gmail.com>
2016-07-04 22:16:01 +0400

    added framapad discussions

---
+++ controllers/actionRoom/ExternalAction.php
@@ -0,0 +1,17 @@
+<?php
+class ExternalAction extends CAction
+{
+    public function run($id)
+    {
+      $controller=$this->getController();
+
+      $room = PHDB::findOne (ActionRoom::COLLECTION, array("_id"=>new MongoId ( $id ) ),array('name','url','parentId','parentType') );
+      if(!isset($room)) 
+          throw new CTKException("Impossible to find this room");
+
+      $controller->renderPartial( "iframe", array( "name" => @$room['name'],
+      												"parentId" => @$room['parentId'],
+      												"parentType" => @$room['parentType'],
+      												 "url" => @$room['url'] )  );
+    }
+}
