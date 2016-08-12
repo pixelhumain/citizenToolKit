eb3880917fbb0bb3d1957146870bda4246c388c2
raphael <raphael_riviere@yahoo.fr>
2016-08-12 11:30:51 +0400

    Ajout d'action Get pour l'api

---
+++ controllers/person/GetAction.php
@@ -0,0 +1,21 @@
+<?php
+class GetAction extends CAction {
+    
+    public function run($id = null, $format = null, $limit=50, $index=0, $tags = null, $multiTags=null , $key = null, $insee = null) {
+		$controller=$this->getController();
+		// Get format
+		if( $format == Translate::FORMAT_SCHEMA)
+          $bindMap = TranslateSchema::$dataBinding_person;
+        else if( $format == Translate::FORMAT_PLP )
+          $bindMap = TranslatePlp::$dataBinding_person;
+        else if( $format == Translate::FORMAT_AS )
+          $bindMap = TranslateActivityStream::$dataBinding_person;
+        else 
+          $bindMap = TranslateCommunecter::$dataBinding_person;
+
+      	$result = Api::getData($bindMap, $format, Person::COLLECTION, $id,$limit, $index, $tags, $multiTags, $key, $insee);
+
+		Rest::json($result);
+		Yii::app()->end();
+    }
+}
