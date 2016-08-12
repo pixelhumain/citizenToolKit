6c735a78ea151a7a9fdf326b42325aac438a2638
raphael <raphael_riviere@yahoo.fr>
2016-08-12 15:02:47 +0400

    Modification pour l'aPI, et la recherche

--- controllers/project/GetAction.php
+++ controllers/project/GetAction.php
@@ -1,10 +1,17 @@
 <?php
-
-class GetAction extends CAction
-{
-    public function run($id=null) {
+class GetAction extends CAction {
+    
+    public function run($id = null, $format = null, $limit=50, $index=0, $tags = null, $multiTags=null , $key = null, $insee = null) {
 		$controller=$this->getController();
-		$organization = Project::getById($id);
-		Rest::json($organization);
+		// Get format
+		if( $format == Translate::FORMAT_SCHEMA)
+        	$bindMap = TranslateSchema::$dataBinding_event;
+      	else
+        	$bindMap = TranslateCommunecter::$dataBinding_event;
+
+      	$result = Api::getData($bindMap, $format, Person::COLLECTION, $id,$limit, $index, $tags, $multiTags, $key, $insee);
+
+		Rest::json($result);
+		Yii::app()->end();
     }
 }
