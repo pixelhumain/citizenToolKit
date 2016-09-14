d5a1416076be597ee5d528f075b89fdc0ff1ec34
Tibor Katelbach <oceatoon@gmail.com>
2016-09-14 22:56:34 +0400

    name regexp to manage accent

--- controllers/search/GlobalAutoCompleteAction.php
+++ controllers/search/GlobalAutoCompleteAction.php
@@ -26,7 +26,8 @@ class GlobalAutoCompleteAction extends CAction
         $query = array();
         
        // if(isset($search) && $search != "")
-        $query = array( "name" => new MongoRegex("/".$search."/i"));
+        $searchRegExp = self::accentToRegex($search);
+        $query = array( "name" => new MongoRegex("/.*{$searchRegExp}.*/i"));
 
         
         /***********************************  TAGS   *****************************************/
@@ -435,5 +436,44 @@ class GlobalAutoCompleteAction extends CAction
 	private function typeWanted($type, $searchType){
 		if($searchType == null) return true;
 		return in_array($type, $searchType);
+	}
+
+
+	/**
+	* Returns a string with accent to REGEX expression to find any combinations
+	* in accent insentive way
+	*
+	* @param string $text The text.
+	* @return string The REGEX text.
+	*/
+
+	static public function accentToRegex($text)
+	{
+
+		$from = str_split(utf8_decode('ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËẼÌÍÎÏĨÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëẽìíîïĩðñòóôõöøùúûüýÿ'));
+		$to   = str_split(strtolower('SOZsozYYuAAAAAAACEEEEEIIIIIDNOOOOOOUUUUYsaaaaaaaceeeeeiiiiionoooooouuuuyy'));
+		//‘ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËẼÌÍÎÏĨÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëẽìíîïĩðñòóôõöøùúûüýÿaeiouçAEIOUÇ';
+		//‘SOZsozYYuAAAAAAACEEEEEIIIIIDNOOOOOOUUUUYsaaaaaaaceeeeeiiiiionoooooouuuuyyaeioucAEIOUÇ';
+		$text = utf8_decode($text);
+		$regex = array();
+
+		foreach ($to as $key => $value)
+		{
+			if (isset($regex[$value]))
+				$regex[$value] .= $from[$key];
+			else 
+				$regex[$value] = $value;
+		}
+
+		foreach ($regex as $rg_key => $rg)
+		{
+			$text = preg_replace("/[$rg]/", "_{$rg_key}_", $text);
+		}
+
+		foreach ($regex as $rg_key => $rg)
+		{
+			$text = preg_replace("/_{$rg_key}_/", "[$rg]", $text);
+		}
+		return utf8_encode($text);
 	}
 }
