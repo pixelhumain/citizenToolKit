c4a38e14df35ca71a2a5a3169516eb190a38282d
Clément Damiens <clement.damiens@gmail.com>
2016-10-14 15:24:16 +0200

    fix bugs on gallery [network]

--- controllers/gallery/IndexAction.php
+++ controllers/gallery/IndexAction.php
@@ -11,27 +11,27 @@ class IndexAction extends CAction
 		//TODO SBAR - it's not beautifull. Refactor soon
 		if($type == Person::COLLECTION){
 			$controllerId = "person";
- 			$params["person"] = Person::getPublicData($id);
-			/*$params["parent"] = Person::getPublicData($id);
-			$params["controller"] = Person::CONTROLLER;*/
+ 			//$params["person"] = Person::getPublicData($id);
+			$params["parent"] = Person::getPublicData($id);
+			$params["controller"] = Person::CONTROLLER;
 		}
 		else if ($type == Organization::COLLECTION){
 			$controllerId = "organization";
-			$params["organization"] = Organization::getPublicData($id);
-			/*$params["parent"] = Organization::getPublicData($id);
-			$params["controller"] = Organization::CONTROLLER;*/
+			//$params["organization"] = Organization::getPublicData($id);
+			$params["parent"] = Organization::getPublicData($id);
+			$params["controller"] = Organization::CONTROLLER;
 		}
 		else if ($type == Project::COLLECTION){
 			$controllerId = "project";
- 			$params["project"] = Project::getPublicData($id);
-			/*$params["parent"] = Project::getPublicData($id);
-			$params["controller"] = Project::CONTROLLER;*/
+ 			//$params["project"] = Project::getPublicData($id);
+			$params["parent"] = Project::getPublicData($id);
+			$params["controller"] = Project::CONTROLLER;
 		}
 		else if ($type == Event::COLLECTION){
 			$controllerId = "event";
- 			$params["event"] = Event::getPublicData($id);
-			/*$params["parent"] = Event::getPublicData($id);
-			$params["controller"] = Event::CONTROLLER;*/
+ 			//$params["event"] = Event::getPublicData($id);
+			$params["parent"] = Event::getPublicData($id);
+			$params["controller"] = Event::CONTROLLER;
 		}
 		else{
 			throw new CTKException("Impossible to manage this type ".$type);
