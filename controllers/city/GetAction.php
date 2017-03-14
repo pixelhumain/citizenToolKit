0ef480f77663cc5f5db492930993722bc18f77c9
GrondinDam <d.grondin@rt-iut.re>
2017-03-14 13:54:56 +0400

    Add format KML and GeoJson to the API

--- controllers/city/GetAction.php
+++ controllers/city/GetAction.php
@@ -8,10 +8,27 @@ class GetAction extends CAction
 		/*if( $format == Translate::FORMAT_SCHEMA)
 	        $bindMap = TranslateSchema::$dataBinding_city;
 		else*/
-	        $bindMap = TranslateCommunecter::$dataBinding_city;
+
+		if ($format == Translate::FORMAT_KML) {
+			$bindMap = TranslateKml::$dataBinding_city;
+		}
+		elseif ($format == Translate::FORMAT_GEOJSON) {
+		 	$bindMap = TranslateGeoJson::$dataBinding_city;
+		} 
+
+		else {
+	    	$bindMap = TranslateCommunecter::$dataBinding_city;
+		}
+      	
+
       	$result = Api::getData($bindMap, $format, City::COLLECTION, $id,$limit, $index, $tags, $multiTags, $key, $insee);
 
+    if ($format == Translate::FORMAT_KML) {
+	    $strucKml = News::getStrucKml();    
+	    Rest::xml($result, $strucKml,$format);
+    } else
 		Rest::json($result);
+
 		Yii::app()->end();
     }
 }
