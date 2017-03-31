cf51cd7acc752d549b240cf16af3e926edff1492
Raphael <raphael_riviere@yahoo.fr>
2017-03-31 14:01:31 +0400

    Nettoyage code Damien

--- controllers/organization/GetAction.php
+++ controllers/organization/GetAction.php
@@ -6,25 +6,21 @@ class GetAction extends CAction
 		$controller=$this->getController();
 		// Get format
 
-		if( $format == Translate::FORMAT_SCHEMA) {
-	        $bindMap = (empty($id)?TranslateSchema::$dataBinding_allOrganization:TranslateSchema::$dataBinding_organization);
-		} 
-		elseif ($format == Translate::FORMAT_KML) {
-			$bindMap = (empty($id)?TranslateKml::$dataBinding_allOrganization:TranslateKml::$dataBinding_organization);
-		}
-		elseif ($format == Translate::FORMAT_GEOJSON) {
-			$bindMap = (empty($id)?TranslateGeoJson::$dataBinding_allOrganization:TranslateGeoJson::$dataBinding_organization);
-		}
+		if( $format == Translate::FORMAT_SCHEMA)
+	        $bindMap = (empty($id) ? TranslateSchema::$dataBinding_allOrganization : TranslateSchema::$dataBinding_organization);
+		else if ($format == Translate::FORMAT_KML)
+			$bindMap = (empty($id) ? TranslateKml::$dataBinding_allOrganization : TranslateKml::$dataBinding_organization);
+		else if ($format == Translate::FORMAT_GEOJSON) 
+			$bindMap = (empty($id) ? TranslateGeoJson::$dataBinding_allOrganization : TranslateGeoJson::$dataBinding_organization);
 		else
-	       $bindMap = (empty($id)?TranslateCommunecter::$dataBinding_allOrganization:TranslateCommunecter::$dataBinding_organization);
+	       $bindMap = (empty($id) ? TranslateCommunecter::$dataBinding_allOrganization : TranslateCommunecter::$dataBinding_organization);
 
       	$result = Api::getData($bindMap, $format, Organization::COLLECTION, $id,$limit, $index, $tags, $multiTags, $key, $insee);
-
 
       	if ($format == Translate::FORMAT_KML) {
 			$strucKml = News::getStrucKml();		
 			Rest::xml($result, $strucKml,$format);
-		} elseif ($format == "csv") {
+		} else if ($format == "csv") {
 			$res = $result["entities"];
 			$head = Export::toCSV($res, ";", "'");
 		} else
