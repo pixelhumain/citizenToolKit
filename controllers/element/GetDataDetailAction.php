e4bfb7b359efbde4cd42d2efeffb1f63ab54ae0d
Kgneo <tristan.goguet@gmail.com>
2017-04-07 17:21:53 +1100

    up

--- controllers/element/GetDataDetailAction.php
+++ controllers/element/GetDataDetailAction.php
@@ -6,33 +6,60 @@ class GetDataDetailAction extends CAction {
 */
     public function run($type, $id, $dataName) { 
     	//$controller=$this->getController();
+
+    	$contextMap = array();
 		$element = Element::getByTypeAndId($type, $id);
 
-		if($dataName == "community"){
+		if($dataName == "links"){
 			$links=@$element["links"];
 			$contextMap = Element::getAllLinks($links,$type, $id);
-			return Rest::json($contextMap);
-			Yii::app()->end();
 		}
 
-
-		if($dataName == "collection"){
-
+		if($dataName == "events"){ //var_dump($element["links"]); exit;
+			if(isset($element["links"]["events"])){
+				foreach ($element["links"]["events"] as $keyEv => $valueEv) {
+					 $event = Event::getSimpleEventById($keyEv);
+					 //var_dump($event); exit;
+					 if(!empty($event)){
+					 	$event["typeEvent"] = @$event["type"];
+						$event["type"] = "events";
+						$event["typeSig"] = Event::COLLECTION;
+						$contextMap[$keyEv] = $event;
+					 }
+				}
+			}
 		}
 
-
+		if($dataName == "projects"){
+			foreach ($element["links"]["projects"] as $keyProj => $valueProj) {
+				$project = Project::getPublicData($keyProj);
+           		$contextMap[$keyProj] = $project;
+			}
+		}
+		
 		if($dataName == "needs"){
-
+			if(isset($element["links"]["needs"])){
+				foreach ($element["links"]["needs"] as $keyNeed => $value){
+					$need = Need::getSimpleNeedById($keyNeed);
+	           		$contextMap[$keyNeed] = $need;
+				}
+			}
 		}
 
 
 		if($dataName == "poi"){
+			$contextMap = Poi::getPoiByIdAndTypeOfParent($id, $type);
 
 		}
+
+		return Rest::json($contextMap);
+		Yii::app()->end();
 
 
 		
 	}
 }
+
+
 
 ?>
--- models/Search.php
+++ models/Search.php
@@ -581,7 +581,11 @@ class Search {
 	/***********************************  CLASSIFIED   *****************************************/
 	public static function searchClassified($query, $indexStep, $indexMin, $priceMin, $priceMax, $devise){
 
+		$queryPrice = array('$and' =>	array()) ;
+		
+		if(@$priceMin || @$priceMax)
 		$queryPrice = array('$and' =>	array(array('devise' => $devise)) ) ;
+		
 		if(@$priceMin) $queryPrice['$and'][] = array('price' => array('$gte' => (int)$priceMin));
 		if(@$priceMax) $queryPrice['$and'][] = array('price' => array('$lte' => (int)$priceMax));
 		$query = array('$and' => array( $query , $queryPrice) );
