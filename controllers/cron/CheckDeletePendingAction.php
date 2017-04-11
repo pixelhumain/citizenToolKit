328b2f53972f4bb47f5ca96a5d816daee3d74db6
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-04-11 18:00:44 +0400

    add force delete pending

--- controllers/cron/CheckDeletePendingAction.php
+++ controllers/cron/CheckDeletePendingAction.php
@@ -2,14 +2,15 @@
 class CheckDeletePendingAction extends CAction {
 	
 	//Check if elements need to be deleted when the delay for admins to stop the delete is over
-	public function run() {
+	//If $forceDelete is set to true : all the pending delete organization will be deleted without checking the date
+	public function run($forceDelete=false) {
 		$where = array("status" => "deletePending");
 		$type2check = array(Organization::COLLECTION, Project::COLLECTION, Event::COLLECTION);
 		
 		foreach ($type2check as $type) {
 			$elementList = PHDB::find($type,$where);
 			foreach ($elementList as $id => $element) {
-				if ($this->canBeDeleted($element)) {
+				if ($this->canBeDeleted($element) || $forceDelete) {
 					$res = Element::deleteElement($type, $id, @$element["reasonDelete"], @$element["userAskingToDelete"]);
 					if (!$res["result"]) {
 						error_log("Error deleting the element ".$id."of type ".$type. ". Reason : ".$res["msg"]);
@@ -31,7 +32,6 @@ class CheckDeletePendingAction extends CAction {
 		$now = new DateTime("now");
 		$dateToBeDeleted = $element["statusDate"]->toDateTime()->add(new DateInterval('P'.Element::NB_DAY_BEFORE_DELETE.'D'));
 		//error_log("check with date :".$dateToBeDeleted->format(DateTime::ISO8601));
-		return true;
 		return $now > $dateToBeDeleted;
 	}
 }
