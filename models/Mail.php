6712af67f927b63ad67b3d27782acb76c77faa9f
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-05-15 16:22:21 +0400

    add object type on delete mail

--- models/Mail.php
+++ models/Mail.php
@@ -384,14 +384,15 @@ class Mail {
                     "subject" => "[".Yii::app()->name."] - Suppression de ".@$element["name"],
                     "from"=>Yii::app()->params['adminEmail'],
                     "to" => $aPerson["email"],
-                    "tplParams" => array(   
+                    "tplParams" => array(
+                        "elementType" => @$elementType,
                         "elementName" => @$element["name"],
                         "userName" => @$user["name"],
                         "logo"=> Yii::app()->params["logoUrl"],
                         "logo2" => Yii::app()->params["logoUrl2"],
                         "reason" => $reason,
                         "nbDayBeforeDelete" => $nbDayBeforeDelete,
-                        "url" => Yii::app()->getRequest()->getBaseUrl(true)."/".$url,)
+                        "url" => Yii::app()->getRequest()->getBaseUrl(true)."/".$url),
                 );
                 
                 Mail::schedule($params);
