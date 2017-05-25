b0a84aaf5b592fb1f944a4ed2589663db064efd1
Bouboule <clement.damiens@gmail.com>
2017-05-25 20:50:16 +0200

    delete share news / delete news fix

--- controllers/news/DeleteAction.php
+++ controllers/news/DeleteAction.php
@@ -7,6 +7,16 @@ class DeleteAction extends CAction {
             $res = array("result"=>false, "msg"=>"You must be loggued to delete a comment");
         } else {
         	$res = News::delete($id, Yii::app()->session["userId"], true);
+        	if(@$res["newsUp"] && $_POST["isLive"]){
+        		$params=array(
+    			"news"=>array( $id=>$res["newsUp"]), 
+    			"actionController"=>"save",
+    			"canManageNews"=>true,
+    			"canPostNews"=>true,
+                "nbCol" => 1,
+                "pair" => false);
+				echo $controller->renderPartial("newsPartialCO2", $params,true);
+        	}
         }
 
         return Rest::json( $res );
