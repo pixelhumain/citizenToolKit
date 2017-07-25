0b7dbaa00936d5c012c3128e8327e7fe297980d1
Bouboule <clement.damiens@gmail.com>
2017-07-25 21:32:22 +0200

    get notification by updated and remove second foreach to order

--- models/ActivityStream.php
+++ models/ActivityStream.php
@@ -31,7 +31,7 @@ class ActivityStream {
 	public static function getWhere($params) {
 	  	 return PHDB::find( self::COLLECTION,$params,null,null);
 	}
-	public static function getNotifications($param,$sort=array("updated"=>-1,"created"=>-1))
+	public static function getNotifications($param,$sort=array("updated"=>-1))
 	{
 	    return PHDB::findAndSort(self::COLLECTION, $param,$sort);
 	}
