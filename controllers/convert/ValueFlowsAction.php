980cfa2dcfdd12f5164b3507c257cdeea1fd9d45
GrondinDam <d.grondin@rt-iut.re>
2017-08-22 09:18:32 +0400

    Clean convert valueflows

--- controllers/convert/ValueFlowsAction.php
+++ controllers/convert/ValueFlowsAction.php
@@ -3,7 +3,6 @@ class ValueFlowsAction extends CAction {
 
     public function run($url=null, $text_filter=null) {
 
-
 		$res = Convert::convertValueFlowsToPh($url, $text_filter);
 
   		if (isset($res)) {
