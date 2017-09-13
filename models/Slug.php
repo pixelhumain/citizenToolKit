03c1cae6eb21fcbb69f6e3bdbf734aed60463db1
Bouboule <clement.damiens@gmail.com>
2017-09-13 18:32:43 +0200

    hotfix slug

--- models/Slug.php
+++ models/Slug.php
@@ -30,7 +30,7 @@ class Slug {
 		else 
 			return true;
 	}
-	public static function checkAndCreateSlug($str,$type,$id){
+	public static function checkAndCreateSlug($str){
 		$unwanted_array = array(    
 			'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
             'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
