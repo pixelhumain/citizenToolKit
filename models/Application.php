f029dadfaf18732328d3bbccd18eee190b21d651
Oceatoon <oceatoon@gmail.com>
2017-11-02 10:11:54 +0400

    new models

---
+++ models/Application.php
@@ -0,0 +1,34 @@
+<?php
+
+class Application {
+	const COLLECTION = "applications";
+	
+
+	public static function loadDBAppConfig($key = null) {
+		
+		if(!$key){
+			$key = "devParams";
+			if( stripos($_SERVER['SERVER_NAME'], "127.0.0.1") === false && 
+				stripos($_SERVER['SERVER_NAME'], "localhost") === false && 
+				stripos($_SERVER['SERVER_NAME'], "::1") === false && 
+				stripos($_SERVER['SERVER_NAME'], "localhost:8080") === false && 
+				strpos($_SERVER['SERVER_NAME'], "local.")!==0 &&
+				strpos($_SERVER['SERVER_NAME'], "dev.")!==0 )
+			    $key = "prodParams"; // PROD
+		}
+		$params = PHDB::findOne( self::COLLECTION, array( "key" => $key ));
+		if(!$params)
+			throw new CHttpException(403,Yii::t('error','Missing Configs db.applications.key == '.$key.'exists'));
+		else {
+			//load into application params main config map
+			Yii::app()->params["mangoPay"] = array(
+				"ClientId" => $params["mangoPay"]["ClientId"],
+				"ClientPassword" => $params["mangoPay"]["ClientPassword"],
+				"TemporaryFolder" => $params["mangoPay"]["TemporaryFolder"]
+			);
+			
+		}
+    }
+
+}
+?>
