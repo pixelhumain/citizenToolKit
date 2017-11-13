13283b939d88546124ae1ce91318abcc2a477ea3
Bouboule <clement.damiens@gmail.com>
2017-11-13 21:51:44 +0100

    hotfix update organizor event

--- models/Element.php
+++ models/Element.php
@@ -2140,7 +2140,9 @@ class Element {
 				$organizer["organizerId"] = $params["organizerId"] ;
 				$organizer["organizerType"] = $params["organizerType"] ;
 				$resOrg = self::updateField($collection, $id, "organizer", $organizer);
-				$resOrg["value"]["organizer"] = Element::getByTypeAndId( $params["organizerType"], $params["organizerId"]);
+				if($params["organizerType"]!="dontKnow"){
+					$resOrg["value"]["organizer"] = Element::getByTypeAndId( $params["organizerType"], $params["organizerId"]);
+				}
 				$res[] = $resOrg;
 			}
 
