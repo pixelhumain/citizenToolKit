569a57814a31aa9125cc3d1e749f3e58001b9de2
codev <raphaelriviere06@gmail.com>
2017-12-07 15:32:21 +0400

    Modif

--- controllers/organization/GetAction.php
+++ controllers/organization/GetAction.php
@@ -5,7 +5,7 @@ class GetAction extends CAction
     public function run($id = null, $format = null, $limit=50, $index=0, $tags = null, $multiTags=null , $key = null, $insee = null) {
 		$controller=$this->getController();
 		// Get format
-
+		header("Access-Control-Allow-Origin: *");
 		if( $format == Translate::FORMAT_SCHEMA)
 	        $bindMap = (empty($id) ? TranslateSchema::$dataBinding_allOrganization : TranslateSchema::$dataBinding_organization);
 		else if ($format == Translate::FORMAT_KML)
