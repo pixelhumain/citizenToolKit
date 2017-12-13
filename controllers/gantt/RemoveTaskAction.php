9e987c806f8ff0432fe0543ed3ca440eb04e1b19
alexis <alexis@ckiw.me>
2017-12-13 13:12:44 +0100

    cleaning

--- controllers/gantt/RemoveTaskAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-class RemoveTaskAction extends CAction
-{
-    public function run($taskId,$parentType,$parentId) {
-		$controller=$this->getController();
-		$res = Gantt::removeTask($taskId,$parentType,$parentId);
-		return Rest::json($res);
-	}
-}
--- controllers/graph/GetDataAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-class GetDataAction extends CAction
-{
-    public function run($id, $type)
-    {
-        $item = PHDB::findOne( $type ,array("_id"=>new MongoId($id)));
-  		echo Rest::json($item);
-    }
-}
