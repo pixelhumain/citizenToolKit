aa36b5d99f21cd6c7a4507ae23026fb1d598d79b
codev <raphaelriviere06@gmail.com>
2018-03-06 17:28:00 +0400

    Hotfix image ressources

--- models/Element.php
+++ models/Element.php
@@ -319,6 +319,8 @@ class Element {
 			$element = Place::getById($id);
 		else if($type == Classified::COLLECTION || $type == Classified::MODULE)
 			$element = Classified::getById($id);
+		else if($type == Ressource::COLLECTION || $type == Ressource::MODULE)
+			$element = Ressource::getById($id);
 		else if($type == ActionRoom::COLLECTION_ACTIONS)
 			$element = PHDB::findOne( ActionRoom::COLLECTION_ACTIONS ,array("_id"=>new MongoId($id)));
 		else if($type == Survey::CONTROLLER )
@@ -2396,6 +2398,7 @@ class Element {
 
 
 	public static function getInfoDetail($params, $element, $type, $id){
+
 		$params["edit"] = Authorisation::canEditItem(Yii::app()->session["userId"], $type, $id);
         $params["openEdition"] = Authorisation::isOpenEdition($id, $type, @$element["preferences"]);
         $params["controller"] = self::getControlerByCollection($type);
@@ -2406,6 +2409,8 @@ class Element {
         }
         else
         	$links=@$element["links"];
+
+
         $connectType = @self::$connectTypes[$type];
         if($type==Person::COLLECTION)
         	$connectType="friends";
