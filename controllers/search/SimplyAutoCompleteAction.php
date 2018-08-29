898ec85eacd722b8cd8290a7f617fae30baa5343
codev <raphaelriviere06@gmail.com>
2018-08-29 10:15:52 +0400

    Hotfix pour afficher les events lier a l'orga network

--- controllers/search/SimplyAutoCompleteAction.php
+++ controllers/search/SimplyAutoCompleteAction.php
@@ -69,6 +69,11 @@ class SimplyAutoCompleteAction extends CAction
 				if(strcmp($filter, Event::COLLECTION) != 0 && Search::typeWanted(Event::COLLECTION, $searchType)){
 					$q = array("links.attendees.".$parent["id"]=> array('$exists' => 1) );
 					$query = Search::concatQuery($query, $q, '$or');
+					$q = array("links.organizer.".$parent["id"]=> array('$exists' => 1) );
+					$query = Search::concatQuery($query, $q, '$or');
+					$q = array("organizerId" => $parent["id"] );
+					$query = Search::concatQuery($query, $q, '$or');
+					//Rest::json($query);exit;
 				}
 			}
 			
